bce0b384e258d36d9807255b8e90cdbc7917ef7d
Denis Kalbasov <kds-ru@list.ru>
2025-07-25 09:43:41 +1000

    config

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,7 @@
-package cmd
+package main
+
+import "github.com/DeneesK/packet-manager/internal/config"
+
+func main() {
+	conf := config.MustLoad("./")
+}
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,28 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Host      string `yaml:"host"`
+	User      string `yaml:"user"`
+	Key       string `yaml:"key"`
+	RemoteDir string `yaml:"remote_dir"`
+}
+
+func MustLoad(path string) *Config {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+	cfg := &Config{}
+	err = yaml.Unmarshal(data, cfg)
+	if err != nil {
+		log.Fatalf("Failed to decode config: %v", err)
+	}
+	return cfg
+}
