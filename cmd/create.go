a2aee0150ab9dbdb125a6f198ee1b176e1aa200b
Denis Kalbasov <kds-ru@list.ru>
2025-07-25 10:11:03 +1000

    archiver

--- cmd/create.go
+++ cmd/create.go
@@ -1,15 +1,47 @@
 package cmd
 
 import (
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"github.com/DeneesK/packet-manager/internal/archiver"
+	"github.com/DeneesK/packet-manager/internal/collector"
+	"github.com/DeneesK/packet-manager/internal/config"
+	"github.com/DeneesK/packet-manager/internal/parser"
+	"github.com/DeneesK/packet-manager/internal/ssh"
 	"github.com/spf13/cobra"
 )
 
 var createCmd = &cobra.Command{
-	Use:   "create <packet.json>/<packet.yaml>/",
+	Use:   "create <packet.json>",
 	Short: "Create archive and upload",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return nil
+		pf, err := parser.ParsePacketFile(args[0])
+		if err != nil {
+			return err
+		}
+
+		files, err := collector.CollectFiles(pf.Targets)
+		if err != nil {
+			return err
+		}
+		log.Printf("Found %d files\n", len(files))
+
+		archiveName := fmt.Sprintf("%s-%s.zip", pf.Name, pf.Version)
+		err = archiver.CreateZip(files, archiveName)
+		if err != nil {
+			return err
+		}
+		cfg := config.MustLoad("./config.yaml")
+
+		client := &ssh.SSHClient{
+			Host: cfg.Host, User: cfg.User, Key: cfg.Key,
+		}
+		remotePath := filepath.Join(cfg.RemoteDir, archiveName)
+		fmt.Println("Uploading:", archiveName)
+		return client.Upload(archiveName, remotePath)
 	},
 }
 
--- internal/archiver/archiver.go
+++ internal/archiver/archiver.go
@@ -1,1 +1,113 @@
 package archiver
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func CreateZip(files []string, dest string) error {
+	outFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer outFile.Close()
+
+	zipWriter := zip.NewWriter(outFile)
+	defer zipWriter.Close()
+
+	for _, file := range files {
+		err := addFileToZip(zipWriter, file)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func addFileToZip(zipWriter *zip.Writer, filename string) error {
+	fileToZip, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+	defer fileToZip.Close()
+
+	info, err := fileToZip.Stat()
+	if err != nil {
+		return err
+	}
+
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		return err
+	}
+
+	header.Name = filepath.ToSlash(filename)
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(writer, fileToZip)
+	return err
+}
+
+func ExtractZip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer r.Close()
+
+	for _, f := range r.File {
+		fpath := filepath.Join(dest, f.Name)
+
+		rel, err := filepath.Rel(dest, fpath)
+		if err != nil {
+			return err
+		}
+		if strings.HasPrefix(rel, "..") || strings.Contains(rel, string(os.PathSeparator)+"..") {
+			return fmt.Errorf("illegal file path: %s", fpath)
+		}
+
+		if f.FileInfo().IsDir() {
+			err := os.MkdirAll(fpath, f.Mode())
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		if err := os.MkdirAll(filepath.Dir(fpath), 0755); err != nil {
+			return err
+		}
+
+		outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+		if err != nil {
+			return err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			outFile.Close()
+			return err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
